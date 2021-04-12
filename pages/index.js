f0455ba418865ab972d29eec351d600cdec0df3d
Perry Fustero <pjfust@gmail.com>
2021-04-12 11:36:34 -0400

    more gifs

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import Image from 'next/image'
+import { baseUrl, miscImages } from '../constants'
 import Layout from '../components/layout'
 import CenterColumn from '../components/centerColumn'
 import styles from '../styles/pages/home.module.css'
@@ -26,47 +28,62 @@ export default function Home() {
 	return (
 		<Layout location="home">
 			<div className={styles.homeContainer}>
-				<div>
-					<div className={styles.homeTop}>
-						<div className={styles.sideColumn}>
-							{
-								// <img
-								// 	className={`imgHorizontalFlip ${horseSpin}`}
-								// 	src="icons/horsewalk-compress-transparent.gif"
-								// 	alt="horse"
-								// 	height="180"
-								// 	onClick={this.horseSpin}
-								// 	onMouseOver={this.handleHorseHover}
-								// 	onMouseOut={this.handleHorseUnHover}
-								// />
-							}
-						</div>
-						<CenterColumn
-							handleInvertColors={handleInvertColors}
-							handleTitleColor={handleTitleColor}
-							// handleTikiClick={this.handleTikiClick}
-							invertProfile={invertProfile}
-							titleColor={titleColor}
-							// leftTikiHue={leftTikiHue}
-							// rightTikiHue={rightTikiHue}
-							// clickCount={clickCount}
-							// customTheme={customTheme}
+				<div className={styles.homeMain}>
+					<div className={styles.sideColumn}>
+          <Image
+							width={200}
+							height={200}
+							alt="cat walk"
+							src={`${baseUrl}/${miscImages.horseHead.folder}/${miscImages.horseHead.src}`}
+              loader={({ src }) => src}
 						/>
-						<div className={styles.sideColumn}>
-							{
-								// <img
-								// 	className={`imgHorizontalFlip cat ${catSpin}`}
-								// 	src="images/cat-compress.gif"
-								// 	alt="cat walk"
-								// 	onClick={this.catSpin}
-								// />
-							}
-						</div>
-					</div>
-					{/* <p className="dumbText">{dumbText}</p> */}
 
-					{/* <Ticker /> */}
+						{
+							// <img
+							// 	className={`imgHorizontalFlip ${horseSpin}`}
+							// 	src="icons/horsewalk-compress-transparent.gif"
+							// 	alt="horse"
+							// 	height="180"
+							// 	onClick={this.horseSpin}
+							// 	onMouseOver={this.handleHorseHover}
+							// 	onMouseOut={this.handleHorseUnHover}
+							// />
+						}
+					</div>
+					<CenterColumn
+						handleInvertColors={handleInvertColors}
+						handleTitleColor={handleTitleColor}
+						// handleTikiClick={this.handleTikiClick}
+						invertProfile={invertProfile}
+						titleColor={titleColor}
+						// leftTikiHue={leftTikiHue}
+						// rightTikiHue={rightTikiHue}
+						// clickCount={clickCount}
+						// customTheme={customTheme}
+					/>
+					<div className={styles.sideColumn}>
+						<Image
+							width={200}
+							height={200}
+							alt="cat walk"
+							src={`${baseUrl}/${miscImages.cat.folder}/${miscImages.cat.src}`}
+              loader={({ src }) => src}
+              className={styles.imgVerticalFlip}
+						/>
+
+						{
+							// <img
+							// 	className={`imgHorizontalFlip cat ${catSpin}`}
+							// 	src="images/cat-compress.gif"
+							// 	alt="cat walk"
+							// 	onClick={this.catSpin}
+							// />
+						}
+					</div>
 				</div>
+				{/* <p className="dumbText">{dumbText}</p> */}
+
+				{/* <Ticker /> */}
 			</div>
 		</Layout>
 	)
