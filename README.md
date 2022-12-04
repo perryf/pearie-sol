c4a49d58c60759b39d3f3379336cef38bae74f05
Perry Fustero <pjfust@gmail.com>
2022-12-04 15:30:53 -0500

    remove upcoming show string, reformat markdown readme

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # PEARIE SOL
 
-- https://peariesol.com/
+- <https://peariesol.com/>
 
 ## How to Release updates
 
 - Push to main! - no need to run deploy, Vercel does it for you
-- Confirm changes at `https://peariesol.com/`
+- Confirm changes at <https://peariesol.com/>
 
 <!-- custom domain: peariesol.com -->
 <!-- enforce https -->
 
 ## Resources
 
-- https://gifcities.org/
-- http://www.rw-designer.com/gallery
-- https://hackernoon.com/how-to-set-up-godaddy-domain-with-github-pages-a9300366c7b
+- <https://gifcities.org/>
+- <http://www.rw-designer.com/gallery>
+- <https://hackernoon.com/how-to-set-up-godaddy-domain-with-github-pages-a9300366c7b>
--- data/tickerInfo.js
+++ data/tickerInfo.js
@@ -1,14 +1,15 @@
 /* 
   // * For the ticker to work best, make sure your string is not too short and not too long.  See examples below
 */
+let upcomingShowsStr = ''
 
 // const happinessStr = 'New album REAL HAPPINESS out June 18th on HHBTM Records!'
-// const upcomingShowsStr =
+//  upcomingShowsStr =
 // 	'4/20 @ the Runaway with Josephine Network & Shawnis and the Shimmers'
-// const upcomingShowsStr =
+//  upcomingShowsStr =
 // 	'🚨🚨 Upcoming Show: 5/14 at Comet Ping Pong (w/ Evolfo)! 🚨🚨'
-const upcomingShowsStr =
-	'🚨🚨 Upcoming Show: 10/29 Rhizome (w/ Past Pawn & Kilynn Lunsford)! 🚨🚨'
+//  upcomingShowsStr =
+// 	'🚨🚨 Upcoming Show: 10/29 Rhizome (w/ Past Pawn & Kilynn Lunsford)! 🚨🚨'
 
 const tickerStr = upcomingShowsStr
 
