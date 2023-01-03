eeb3544ab779922f8e0286e8a63f79fe86452e57
Perry Fustero <pjfust@gmail.com>
2023-01-02 19:33:10 -0500

    add back email images

--- pages/info.js
+++ pages/info.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import { baseUrl, miscImages } from '../constants'
 import Layout from '../components/layout'
 import InfoQuotesLinks from '../components/infoQuotesLinks'
 import EmailBox from '../components/emailBox'
@@ -8,7 +10,35 @@ export default function Info() {
 		<Layout location="info">
 			<div className={styles.infoPage}>
 				<div className={styles.topRow}>
+					<div className={styles.emailImageBox}>
+						<a
+							href="mailto:solpearie@gmail.com?Subject=Hello%20Horsey"
+							target="_top"
+						>
+							<Image
+								width={100}
+								height={60}
+								alt="horse email"
+								src={`${baseUrl}/${miscImages.horseHead.folder}/${miscImages.horseEmailMe.src}`}
+								className={styles.emailImage}
+							/>
+						</a>
+					</div>
 					<EmailBox />
+					<div className={styles.emailImageBox}>
+						<a
+							href="mailto:solpearie@gmail.com?Subject=Hello%20Dolly"
+							target="_top"
+						>
+							<Image
+								width={100}
+								height={60}
+								alt="horse email me"
+								src={`${baseUrl}/${miscImages.horseHead.folder}/${miscImages.horseEmail.src}`}
+								className={styles.emailImage}
+							/>
+						</a>
+					</div>
 				</div>
 				<InfoQuotesLinks />
 			</div>
