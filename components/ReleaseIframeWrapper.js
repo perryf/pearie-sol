6703b52af46de1dd6d53954dd7feb3385cc25360
Perry Fustero <pjfust@gmail.com>
2024-10-04 14:12:53 -0400

    add synthetic life to releases, refactor

---
+++ components/ReleaseIframeWrapper.js
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from 'react'
+import styles from '../styles/pages/releases.module.css'
+
+export default function ReleaseIframeWrapper(props) {
+  const { src, title, id, releaseHref, releaseMessage } = props
+
+  const [loadedIframe, setLoadedIframe] = useState(false)
+  const componentIsMounted = useRef(true)
+
+  useEffect(() => {
+    return () => (componentIsMounted.current = false)
+  }, [])
+
+  return (
+    <div className={styles.iframeBox}>
+      {!loadedIframe && (
+        <div className={styles.loader}>Patience is a virtue...</div>
+      )}
+      <iframe
+        className={styles.iframe}
+        src={src}
+        seamless
+        onLoad={() => {
+          if (componentIsMounted.current) setLoadedIframe(true)
+        }}
+        title={title}
+        id={id}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      />
+      <a href={releaseHref} className={styles.releaseLink}>
+        {releaseMessage}
+      </a>
+    </div>
+  )
+}
--- pages/releases.js
+++ pages/releases.js
@@ -1,72 +1,38 @@
-import { useEffect, useRef, useState } from 'react'
 import Layout from '../components/layout'
+import ReleaseIframeWrapper from '../components/ReleaseIframeWrapper'
 import styles from '../styles/pages/releases.module.css'
 
 export default function Releases() {
-	const [loadedST, setLoadedST] = useState(false)
-	const [loadedRealHappiness, setLoadedRealHappiness] = useState(false)
-	const componentIsMounted = useRef(true)
+  return (
+    <Layout location="releases">
+      <div className={styles.releasesPage}>
+        <ReleaseIframeWrapper
+          src="https://bandcamp.com/EmbeddedPlayer/album=1459803237/size=large/bgcol=333333/linkcol=9a64ff/tracklist=false/transparent=true/"
+          title="Synthetic Life"
+          id="bandCampEmbedSyntheticLife"
+          releaseHref="https://peariesol.bandcamp.com/album/synthetic-life/"
+          releaseMessage="Self Released on Wollumbinia Records"
+          lazy
+        />
 
-	useEffect(() => {
-		return () => (componentIsMounted.current = false)
-	}, [])
+        <ReleaseIframeWrapper
+          src="https://bandcamp.com/EmbeddedPlayer/album=3036115136/size=large/bgcol=333333/linkcol=e55e10/tracklist=false/transparent=true/"
+          title="Real Happiness"
+          id="bandCampEmbedRealHappiness"
+          releaseHref="https://www.hhbtm.com/product/pearie-sol-real-happiness/"
+          releaseMessage="Available on Happy Happy Birthday To Me Records"
+          lazy
+        />
 
-	return (
-		<Layout location="releases">
-			<div
-				className={styles.releasesPage}
-				style={{
-					animationPlayState: loadedST ? 'running' : 'paused'
-				}}
-			>
-				<div className={styles.iframeBox}>
-					{!loadedRealHappiness && (
-						<div className={styles.loader}>Patience is a virtue...</div>
-					)}
-					<iframe
-						className={styles.iframe}
-						src="https://bandcamp.com/EmbeddedPlayer/album=3036115136/size=large/bgcol=333333/linkcol=e55e10/tracklist=false/transparent=true/"
-						seamless
-						onLoad={() => {
-							if (componentIsMounted.current) setLoadedRealHappiness(true)
-						}}
-						title="Real Happiness"
-						id="bandCampEmbedRealHappiness"
-					/>
-					<a
-						href="https://www.hhbtm.com/product/pearie-sol-real-happiness/"
-						className={styles.coolLink}
-					>
-						<p className={styles.releaseLink}>
-							Available on Happy Happy Birthday To Me Records
-						</p>
-					</a>
-				</div>
-
-				<div className={styles.iframeBox}>
-					{!loadedST && (
-						<div className={styles.loader}>Patience is a virtue...</div>
-					)}
-					<iframe
-						className={styles.iframe}
-						src="https://bandcamp.com/EmbeddedPlayer/album=797557584/size=large/bgcol=333333/linkcol=cccccc/tracklist=false/transparent=true/"
-						seamless
-						onLoad={() => {
-							if (componentIsMounted.current) setLoadedST(true)
-						}}
-						title="pearie sol s/t tape"
-						id="bandCampEmbedST"
-					/>
-					<a
-						href="http://sisterpolygonrecords.bigcartel.com/"
-						className={styles.coolLink}
-					>
-						<p className={styles.releaseLink}>
-							Available on Sister Polygon Records
-						</p>
-					</a>
-				</div>
-			</div>
-		</Layout>
-	)
+        <ReleaseIframeWrapper
+          src="https://bandcamp.com/EmbeddedPlayer/album=797557584/size=large/bgcol=333333/linkcol=cccccc/tracklist=false/transparent=true/"
+          title="pearie sol s/t tape"
+          id="bandCampEmbedST"
+          releaseHref="http://sisterpolygonrecords.bigcartel.com/"
+          releaseMessage="Available on Sister Polygon Records"
+          lazy
+        />
+      </div>
+    </Layout>
+  )
 }
